24dda12b10d9a3ba2f81022741dbee1e4f088c67
saurvkmr <contact.xenon@gmail.com>
2021-02-14 11:08:52 +0530

    update

--- src/main/java/practise/string/ValidAnagram.java
+++ src/main/java/practise/string/ValidAnagram.java
@@ -35,26 +35,17 @@ public class ValidAnagram {
 
     // chars will be always small characters
     private static boolean isAnagramUsingCharArray(String s, String t) {
-        int[] chars = new int[26];
-        int a = 97, count, idx;
-        char c;
-        for (int i = 0; i < s.length(); i++) {
-            c = s.charAt(i);
-            idx = c - a;
-            count = chars[idx];
-            chars[idx] = ++count;
+        int[] map = new int[26];
+        for (char c : s.toCharArray()) {
+            map[c]++;
         }
-
-        for (int i = 0; i < t.length(); i++) {
-            c = t.charAt(i);
-            idx = c - a;
-            count = chars[idx];
-            chars[idx] = --count;
+        for (char c : t.toCharArray()) {
+            map[c]--;
         }
-
-        for (int i : chars) {
-            if (i > 0 || i < 0)
+        for (int i : map) {
+            if (i != 0) {
                 return false;
+            }
         }
         return true;
     }
