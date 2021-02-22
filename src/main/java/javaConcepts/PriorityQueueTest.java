e28ddb378054a9d25f4e7917847c8b780e455f06
saurvkmr <contact.xenon@gmail.com>
2021-02-22 11:33:33 +0530

    update

---
+++ src/main/java/javaConcepts/PriorityQueueTest.java
@@ -0,0 +1,24 @@
+package javaConcepts;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class PriorityQueueTest {
+
+    public static void main(String[] args) {
+        Queue q = new PriorityQueue();
+        q.add("a");
+        q.add("A");
+        q.add("Ab");
+        q.add("Ac");
+        q.add("bc");
+        q.add("db");
+        q.add("bd");
+        printQueue(q);
+    }
+
+    private static void printQueue(Queue q) {
+        while(!q.isEmpty())
+            System.out.println(q.poll());
+    }
+}
