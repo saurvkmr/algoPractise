d37851b74eec0cea09112b31e62524ac61730af6
saurvkmr <contact.xenon@gmail.com>
2021-02-22 09:23:08 +0530

    update

--- src/main/java/javaConcepts/ThreadOneAfterAnother.java
+++ src/main/java/javaConcepts/ThreadOneAfterAnother.java
@@ -3,19 +3,19 @@ package javaConcepts;
 public class ThreadOneAfterAnother {
 
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Thread(new WorkerThread("Thread 1"));
-        Thread t2 = new Thread(new WorkerThread("Thread 2", t1));
-        Thread t3 = new Thread(new WorkerThread("Thread 3", t2));
+        Thread one = new Thread(new WorkerThread("Thread one"));
+        Thread two = new Thread(new WorkerThread("Thread two", one));
+        Thread three = new Thread(new WorkerThread("Thread three", two));
 
-        t1.start();
-        t2.start();
-        t3.start();
+        one.start();
+        two.start();
+        three.start();
     }
 
     private static class WorkerThread implements Runnable {
 
         String name;
-        Thread th;
+        Thread parentThread;
 
         public WorkerThread(String name) {
             this.name = name;
@@ -23,18 +23,18 @@ public class ThreadOneAfterAnother {
 
         public WorkerThread(String name, Thread th) {
             this.name = name;
-            this.th = th;
+            this.parentThread = th;
         }
 
         @Override
         public void run() {
             try {
-                if (th != null) th.join();
+                if (parentThread != null) parentThread.join();
             } catch (Exception e) {
             }
 
             for (int i = 0; i < 4; i++)
-                System.out.println("Thread name is " + name);
+                System.out.println(name);
         }
     }
 }
