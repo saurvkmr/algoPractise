06d8d7c0cc49e30b849f5c0d54f4c092fd3bb23a
saurvkmr <contact.xenon@gmail.com>
2021-03-02 23:33:44 +0530

    update

--- src/main/java/practise/leetcode/Candies.java
+++ src/main/java/practise/leetcode/Candies.java
@@ -5,6 +5,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Candies {
+    public static void main(String[] args) {
+        System.out.println(1.0/0.0);
+    }
     public int distributeCandies(int[] candyType) {
         Set<Integer> candies = new HashSet<>();
         Arrays.stream(candyType).forEach(c -> candies.add(c));
