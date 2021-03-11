6039c47985caf444259b4b3c9a1adbe74931f240
saurvkmr <contact.xenon@gmail.com>
2021-03-11 11:39:21 +0530

    update

--- src/main/java/practise/arrays/GoodPairs.java
+++ src/main/java/practise/arrays/GoodPairs.java
@@ -1,11 +1,12 @@
 package practise.arrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
  * A pair (i,j) is called good if nums[i] == nums[j] and i < j.
- *
+ * <p>
  * Return the number of good pairs.
  * Input: nums = [1,2,3,1,1,3]
  * Output: 4
@@ -17,19 +18,21 @@ import java.util.Map;
 public class GoodPairs {
 
     public static void main(String[] args) {
-        int[] arr = {1,1,1,2,2};
+        int[] arr = {1, 1, 1, 2, 2};
         System.out.println(countGoodPairs(arr));
     }
 
     static int countGoodPairs(int[] arr) {
         int pairs = 0;
         Map<Integer, Integer> count = new HashMap<>();
-        for (int i = 0; i < arr.length; i++) {
-            count.put(arr[i], count.getOrDefault(arr[i], 0) + 1);
-        }
-        for(Map.Entry entry : count.entrySet()) {
+        Arrays.stream(arr)
+                .forEach(e -> {
+                    count.computeIfAbsent(e, k -> 0);
+                    count.computeIfPresent(e, (k, v) -> v + 1);
+                });
+        for (Map.Entry entry : count.entrySet()) {
             int val = (Integer) entry.getValue();
-            pairs += (val * (val-1)) /2;
+            pairs += (val * (val - 1)) / 2;
         }
         return pairs;
     }
--- src/main/java/practise/arrays/MaxContigiousSum.java
+++ src/main/java/practise/arrays/MaxContigiousSum.java
@@ -2,13 +2,13 @@ package practise.arrays;
 
 public class MaxContigiousSum {
     public static void main(String[] args) {
-        int[] nums = {1,2,4,5,6,-110,8,9};
+        int[] nums = {1, 2, 4, 5, 6, -110, 8, 9};
         System.out.println(getMaxContigiousSum(nums));
     }
 
     static int getMaxContigiousSum(int[] nums) {
         int maxSum = 0, sum = 0;
-        for(int i = 0; i<nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             if (nums[i] < 0)
                 maxSum = sum;
             sum += nums[i];
